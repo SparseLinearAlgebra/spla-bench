33bfcc7f0c09b840bb7ac6a40f8c4a692d4e3c77
EgorOrachev <egororachyov5@gmail.com>
2022-01-22 17:38:25 +0300

    [Scripts] Add graphblast sssp & tc (resolves #7 resolves #8)

--- scripts/driver_graphblast.py
+++ scripts/driver_graphblast.py
@@ -16,8 +16,12 @@ class DriverGraphBLAST(driver.Driver):
         self.gsssp = "gsssp"
         self.gtc = "gtc"
 
+        # 0: do not display per iteration timing, 1: display per iteration timing
         self.timing = 0
+        # 0: follow mtx, 1: force undirected graph to be directed, 2: force directed graph to be undirected
         self.directed = 0
+        # 0: run CPU verification, 1: skip CPU algorithm verification
+        self.skip_cpu_verify = 0
 
     def run_bfs(self, matrix_path, source_vertex, num_iterations) -> driver.ExecutionResult:
         output = subprocess.check_output([str(self.exec_dir / self.gbfs),
@@ -25,14 +29,28 @@ class DriverGraphBLAST(driver.Driver):
                                           f"--niter={num_iterations}",
                                           f"--timing={self.timing}",
                                           f"--directed={self.directed}",
+                                          f"--skip_cpu_verify={self.skip_cpu_verify}",
                                           str(matrix_path)])
         return DriverGraphBLAST._parse_output(output, num_iterations)
 
     def run_sssp(self, matrix_path, source_vertex, num_iterations) -> driver.ExecutionResult:
-        pass
+        output = subprocess.check_output([str(self.exec_dir / self.gsssp),
+                                          f"--source={source_vertex}",
+                                          f"--niter={num_iterations}",
+                                          f"--timing={self.timing}",
+                                          f"--directed={self.directed}",
+                                          f"--skip_cpu_verify={self.skip_cpu_verify}",
+                                          str(matrix_path)])
+        return DriverGraphBLAST._parse_output(output, num_iterations)
 
     def run_tc(self, matrix_path, num_iterations, directed) -> driver.ExecutionResult:
-        pass
+        output = subprocess.check_output([str(self.exec_dir / self.gsssp),
+                                          f"--niter={num_iterations}",
+                                          f"--timing={self.timing}",
+                                          f"--directed={self.directed}",
+                                          f"--skip_cpu_verify={self.skip_cpu_verify}",
+                                          str(matrix_path)])
+        return DriverGraphBLAST._parse_output(output, num_iterations)
 
     @staticmethod
     def _parse_output(output, n):
